2efc1baaa53d71978118cab1da1eb80e49a99a7d
Raimo Radczewski <raimo.radczewski@vaamo.de>
2016-10-03 21:46:11 +0200

    Add pending test for predicate

--- test/projection.test.js
+++ test/projection.test.js
@@ -1,4 +1,4 @@
-import { on, projection } from '../src';
+import { on, projection, event, EventStore, InMemoryStorageBackend } from '../src';
 import expect from 'expect';
 
 describe('projection', () => {
@@ -14,12 +14,31 @@ describe('projection', () => {
   });
 });
 
+const propEq = (propName, value) => obj => obj[propName] === value;
+
 describe('on', () => {
   it('provides a shorthand to build event handlers', () => {
     const eventHandler = on('FOO', (s, m) => s+m);
 
     expect(eventHandler.type).toEqual('FOO');
     expect(eventHandler.fold('FOO', 'BAR')).toEqual('FOOBAR');
+  });
+
+  xit('can also take a predicate for applying the fold', () => {
+    const eventStore = new EventStore(InMemoryStorageBackend());
+
+    const Foo = event('FOO');
+    eventStore.storeEvent(Foo({ userId: '124', userName: 'shouldnt_show_up'}));
+    eventStore.storeEvent(Foo({ userId: '123', userName: 'should_show_up'}));
+
+    const someProjection = projection(
+      on("FOO", propEq("userId", "123"), (state, {userName}) => state.concat(userName))
+    )([]);
+
+    return eventStore.project(someProjection)
+      .then(result => {
+        expect(result).toEqual(['should_show_up']);
+      });
   });
 });
 
